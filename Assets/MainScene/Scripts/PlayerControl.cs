42e5dac08f9773bcd229a55823971c6562767f3f
Fortyseven <fortyseven@gmail.com>
2014-12-06 02:54:33 -0500

    First draft; terrain, snow particles, snowman

---
+++ Assets/MainScene/Scripts/PlayerControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    private TankMovement treads;
+
+    void Start()
+    {
+        treads = GetComponent<TankMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        treads.RotateLeft( Input.GetAxis( "Left Tread" ) );
+        treads.RotateRight( Input.GetAxis( "Right Tread" ) );
+    }
+}
---
+++ Assets/MainScene/Scripts/TankMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankMovement : MonoBehaviour
+{
+    private const float BASE_SPEED = 0.5f;
+    private const float FOWARD_SPEED = 0.05f;
+
+    public void RotateLeft( float amount )
+    {
+        transform.Rotate( Vector3.up, amount * BASE_SPEED );
+        transform.Translate( new Vector3( 0, 0, amount * BASE_SPEED * FOWARD_SPEED ), Space.Self );
+    }
+
+    public void RotateRight( float amount )
+    {
+        transform.Rotate( Vector3.up, -amount * BASE_SPEED );
+        transform.Translate( new Vector3( 0, 0, amount * BASE_SPEED * FOWARD_SPEED ), Space.Self );
+    }
+}
