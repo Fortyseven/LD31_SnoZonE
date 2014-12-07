f3f4cc6590fe73437f91f100f0f2d2f3722b6b8a
Fortyseven <fortyseven@gmail.com>
2014-12-06 22:56:30 -0500

    Reticle now changes from green to red when over tagged enemy

--- Assets/MainScene/Scripts/ReticleController.cs
+++ Assets/MainScene/Scripts/ReticleController.cs
@@ -6,7 +6,8 @@ public class ReticleController : MonoBehaviour
 {
     public GameObject ReticleObject;
 
-    public Image _imgReticle;
+    private  Image _imgReticle;
+    public Text  _txtRange;
 
     void Start()
     {
@@ -16,6 +17,17 @@ public class ReticleController : MonoBehaviour
 
     void Update()
     {
+        Vector3 dir = transform.TransformDirection( Vector3.forward );
+        RaycastHit hit_info;
 
+        if ( Physics.Raycast( transform.position, dir, out hit_info, 50.0f ) ) {
+            if ( hit_info.collider.tag == "Enemy" ) {
+                _txtRange.text = hit_info.distance.ToString( "###.#" ) + " M";
+                _imgReticle.color = Color.red;
+                return;
+            }
+        }
+        _txtRange.text = "-- M";
+        _imgReticle.color = Color.green;
     }
 }
