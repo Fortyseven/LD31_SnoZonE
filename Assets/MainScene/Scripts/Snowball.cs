ad88f1624657b2e3dd42fac97f9902ca37a4ccac
Fortyseven <fortyseven@gmail.com>
2014-12-09 04:10:30 -0500

    Sound added to firing of snowball (jeez, guy)

--- Assets/MainScene/Scripts/Snowball.cs
+++ Assets/MainScene/Scripts/Snowball.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Snowball : MonoBehaviour
 {
+    public AudioClip AudioClipFire;
     /* Note: these snowballs should be in an object pool, but we're
      * just instantiating/destroying because I'm in a hurry. */
 
@@ -10,6 +11,7 @@ public class Snowball : MonoBehaviour
 
     void Start()
     {
+        audio.PlayOneShot(AudioClipFire);
         Destroy( this.gameObject, LIFETIME );
     }
 }
