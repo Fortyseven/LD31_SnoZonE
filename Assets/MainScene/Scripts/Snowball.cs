124e19c3b585aefb6aae21a62d08f5664a8abe55
Fortyseven <fortyseven@gmail.com>
2014-12-09 04:45:05 -0500

    Added snowball impact noise

--- Assets/MainScene/Scripts/Snowball.cs
+++ Assets/MainScene/Scripts/Snowball.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Snowball : MonoBehaviour
 {
     public AudioClip AudioClipFire;
+    public AudioClip AudioClipImpact;
     /* Note: these snowballs should be in an object pool, but we're
      * just instantiating/destroying because I'm in a hurry. */
 
@@ -11,7 +12,19 @@ public class Snowball : MonoBehaviour
 
     void Start()
     {
-        audio.PlayOneShot(AudioClipFire);
+        audio.PlayOneShot( AudioClipFire );
         Destroy( this.gameObject, LIFETIME );
+    }
+
+    public void OnCollisionEnter( Collision col )
+    {
+        AudioSource col_audio =  col.gameObject.GetComponent<AudioSource>();
+
+        if ( !col_audio ) {
+            col_audio = this.audio;
+        }
+
+        col_audio.PlayOneShot( AudioClipImpact );
+        //Debug.Log( "OH GOD WHAT HAVE I HIT: " + col.gameObject.name );
     }
 }
